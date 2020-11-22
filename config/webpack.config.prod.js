0c734b4624a89e187934aa9c01eeb37b1285e1e5
Berat <le.appl3@gmail.com>
2020-11-22 17:56:31 +0300

    fix

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -15,7 +15,7 @@ const webpackConfig = merge(commonConfig, {
     mode: 'production',
     output: {
         path: helpers.root('dist'),
-        publicPath: '/joblist/',
+        publicPath: '/job-finder/',
         filename: 'js/[hash].js',
         chunkFilename: 'js/[id].[hash].chunk.js'
     },
